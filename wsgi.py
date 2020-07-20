299406b91e7c1e7ecdae42651311cc3826fdd85b
krishu1501 <krishu1501@gmal.com>
2020-07-21 01:41:28 +0530

    changed runtime.txt

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 import os
 from main.app import app
 
+
 # app.secret_key = os.urandom(16)
-# app.run(debug=True, ssl_context=('./ssl.crt', './ssl.key'))
+if __name__ == '__main__':
+	app.run(debug=True, ssl_context=('./ssl.crt', './ssl.key'))
 
 # app.run('localhost', 5000, debug=True)
