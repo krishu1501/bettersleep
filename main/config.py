9cbd21a7ef9edffa1ed591f1edcd22e5668902f8
krishu1501 <krishu1501@gmal.com>
2020-07-28 02:17:27 +0530

    db path from env var

--- main/config.py
+++ main/config.py
@@ -36,8 +36,8 @@ class ProdConfig(Config):
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, "prod.db")
     # vcap = json.loads(os.environ['VCAP_SERVICES'])
     # db_cred = vcap["dashDB For Transactions"][0]["credentials"]
-    db2info = json.loads(os.environ['VCAP_SERVICES'])['dashDB'][0]
-    db_cred = db2info["credentials"]
+    db2info = json.loads(os.environ['VCAP_SERVICES'])
+    db_cred = db2info["dashDB For Transactions"][0]["credentials"]
     username = db_cred["username"]
     password = db_cred["password"]
     host = db_cred["host"]
@@ -53,24 +53,6 @@ class ProdConfig(Config):
     # payload = "{\"123\":\"%s\"}" % (st)
     # url = "https://nodemcu-2da43.firebaseio.com/.json"
     # resp = requests.patch(url=url, headers=headers, data=payload)
-
-    # print( "from env : %s" % (st) )
-    # # st = st1.replace('null','None')
-    # # VCAP_SERVICES = ast.literal_eval(st1)
-    # VCAP = ast.literal_eval(st)
-    # username = os.environ.get("username")
-    # print("username : ",os.environ.get("username"))
-    # password = os.environ.get("password")
-    # print("password : ",os.environ.get("password"))
-    # print("host : ",os.environ.get("host"))
-    # host = os.environ.get("host")
-    # print("portn : ",os.environ.get("portn"))
-    # print("type : ",type(os.environ.get("portn")))
-    # # port = int(str(os.environ.get("portn")))
-    # port = 50000
-    # print("type : ",type(os.environ.get("portn")))
-    # database = os.environ.get("database")
-    # schema = os.environ.get("schema")
     
     # SQLALCHEMY_DATABASE_URI = f'ibm_db_sa+pyodbc400://{username}:{password}@{host}:{port}/{database};currentSchema={schema}'
     SQLALCHEMY_DATABASE_URI = f'ibm_db_sa://{username}:{password}@{host}:{portn}/{database}'
