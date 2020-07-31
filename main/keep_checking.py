c021b3f0c31186e61a3aacf9a09ac946218bf463
krishu1501 <krishu1501@gmal.com>
2020-07-31 19:09:57 +0530

    new col initialized

--- main/keep_checking.py
+++ main/keep_checking.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
@@ -21,6 +22,7 @@ def new_access_token(refresh_token):
       'grant_type': 'refresh_token',
       'client_id': os.environ.get('CLIENT_ID'),
       'client_secret': os.environ.get('CLIENT_SECRET'),
+      #   'refresh_token':"1//0grv2p2xFhVucCgYIARAAGBASNwF-L9IrlelcE5TTtkyaxnAf1T2UddBCz3egbyGLzPCBlBF1MEd3CgniF6Lauu8jgwLWSsPr0J0"
       'refresh_token': refresh_token
     }
 
@@ -35,7 +37,8 @@ def new_access_token(refresh_token):
 ########################end of refresh token functn ##############################
 
 def toggle_light(nodemcu, state):
-  nodemcu['vars']['LED_STATUS'] = state
+  vars = nodemcu['vars']
+  vars['LED_STATUS'] = state
   headers = {
     'Content-Type': 'application/json'
   }
@@ -53,86 +56,81 @@ def send_time(nodemcu,state):
 	payload = "{\"%s\":\"%s\"}" % (dt_now, state)
 	url = "%s/%s.json" % (nodemcu['url'][:-5], state)
 	resp = requests.patch(url=url, headers=headers, data=payload)
-  
+	# print(resp.json())
+
 ########################   main_user_functn  ###############################
+
+
 
 def start():
 
-	all_user=User.query.all()
-	for i in all_user:
-		if not i.active:
-		  continue
-		  
-		try:
-		    # print("i.nodemcu" , i.nodemcu)
-		    # print("i.token_created_at" , i.token_created_at)
-		    # print("i.tokens" , i.tokens)
-		    # tok = ast.literal_eval(i.tokens)
-		    # print("refresh_token" , tok["refresh_token"])
-		    # tok['refresh_token'] = '1//0gLCiGWGZiCcoCgYIARAAGBASNwF-L9IrBDx2rcyoZIyDuhTZqEcwgG7PUxz2xESgtgdxBaJ6WEACnoD9TmdZw7qr8qCBO7weiiI'
-		    # i.tokens = str(tok)
-		    # db.session.add(i)
-		    # db.session.commit()
-		    # print("access_token" , tok["access_token"])
-		    # print("refresh_token" , tok["refresh_token"])
+  all_user=User.query.all()
+  for i in all_user:
+    if not i.active:
+      continue
+      
+    try:
+        # print("i.nodemcu" , i.nodemcu)
+        # print("i.token_created_at" , i.token_created_at)
+        # print("i.tokens" , i.tokens)
+        # tok = ast.literal_eval(i.tokens)
+        # print("refresh_token" , tok["refresh_token"])
+        # tok['refresh_token'] = '1//0gLCiGWGZiCcoCgYIARAAGBASNwF-L9IrBDx2rcyoZIyDuhTZqEcwgG7PUxz2xESgtgdxBaJ6WEACnoD9TmdZw7qr8qCBO7weiiI'
+        # i.tokens = str(tok)
+        # db.session.add(i)
+        # db.session.commit()
+        # print("access_token" , tok["access_token"])
+        # print("refresh_token" , tok["refresh_token"])
 
-		    # if i.active:
-		    #   continue
-		    tok = ast.literal_eval(i.tokens)
+        # if i.active:
+        #   continue
+        tok = ast.literal_eval(i.tokens)
+        ctime = int(calendar.timegm(time.strptime(str(i.token_created_at), '%Y-%m-%d %H:%M:%S.%f')))
+        # ctime = int(calendar.timegm((i.token_created_at).utctimetuple()))
+        # ctime = (int)((i.token_created_at).timestamp())
+        # ctime is in sec
+        # if atleat 15 min left to expire
+        # print(int(round(time.time())))
+        # print(ctime)
+        # if i.active:
+        #   continue
+        if int(round(time.time())) - ctime < 2700 :
+          # print('access_token not expired')
+          access_token = tok["access_token"]
+        else :
+          # print('getting new access_token')
+          access_token, refresh_token = new_access_token(tok["refresh_token"])
+          i.token_created_at = datetime.utcnow()
+          tok["access_token"] = access_token
+          if refresh_token != tok["refresh_token"]:
+            tok["refresh_token"] = refresh_token
 
-		    # if i.token_created_at:
-		    # 	ctime = int(calendar.timegm(time.strptime(str(i.token_created_at), '%Y-%m-%d %H:%M:%S.%f')))
-		    # else:
-		    # 	ctime = 0
-		    # # ctime = int(calendar.timegm((i.token_created_at).utctimetuple()))
-		    # # ctime = (int)((i.token_created_at).timestamp())
-		    # # ctime is in sec
-		    # # if atleat 15 min left to expire
-		    # # print(int(round(time.time())))
-		    # # print(ctime)
-		    # # if i.active:
-		    # #   continue
-		    # if int(round(time.time())) - ctime < 2700 :
-		    #   # print('access_token not expired')
-		    #   access_token = tok["access_token"]
-		    # else :
-		    #   # print('getting new access_token')
-		    #   access_token, refresh_token = new_access_token(tok["refresh_token"])
-		    #   i.token_created_at = datetime.utcnow()
-		    #   tok["access_token"] = access_token
-		    #   if refresh_token != tok["refresh_token"]:
-		    #     tok["refresh_token"] = refresh_token
+        # print("access_token" , access_token)
+        # print(i.token_created_at)
 
-		    access_token, refresh_token = new_access_token(tok["refresh_token"])
-		    i.token_created_at = datetime.utcnow()
-		    tok["access_token"] = access_token
-		    if refresh_token != tok["refresh_token"]:
-		      tok["refresh_token"] = refresh_token
+        endTimeMillis = int(round(time.time() * 1000))
+        startTimeMillis = endTimeMillis - 3600000
+        # if gFit.is_sleeping(1584157920000, 1584157920001, access_token):
+        if gFit.is_sleeping(startTimeMillis, endTimeMillis, access_token):
+          print("sleeping")
+          i.nodemcu = str( toggle_light(ast.literal_eval(i.nodemcu), 0) )
+          send_time(ast.literal_eval(i.nodemcu), 'sleeping')
+        else:
+          print("Not sleeping")
+          i.nodemcu = str( toggle_light(ast.literal_eval(i.nodemcu), 1) )
+          send_time(ast.literal_eval(i.nodemcu), 'awake')
+        
+        i.tokens = str(tok)
+        db.session.add(i)
+        db.session.commit()
 
-		    # print("access_token" , access_token)
-		    # print(i.token_created_at)
-
-		    endTimeMillis = int(round(time.time() * 1000))
-		    startTimeMillis = endTimeMillis - 1800000
-		    # if gFit.is_sleeping(1584157920000, 1584157920001, access_token):
-		    if gFit.is_sleeping(startTimeMillis, endTimeMillis, access_token):
-		      # print("sleeping")
-		      i.nodemcu = str( toggle_light(ast.literal_eval(i.nodemcu), 0) )
-		      send_time(ast.literal_eval(i.nodemcu), 'sleeping')
-		    else:
-		      # print("Not sleeping")
-		      i.nodemcu = str( toggle_light(ast.literal_eval(i.nodemcu), 1) )
-		      send_time(ast.literal_eval(i.nodemcu), 'awake')
-		    
-		    i.tokens = str(tok)
-		    db.session.add(i)
-		    db.session.commit()
-
-		except:
-			continue
+	except:
+      continue
+    # except Exception as e:
+    #   print(e)
+      
 
 if __name__=='__main__':
-	while (True):
-		start()
-		time.sleep(1800)
-	
+  while (True):
+    start()
+    time.sleep(10)
